31c2e125af4e92a0ffe69eb1f4d78fbb74382b35
Leoni <118344314+lktay@users.noreply.github.com>
2023-02-25 11:53:26 +0000

    setting up work area

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h3>This is an anime to watch list</h3>
+      <p>It utilises the Jikan API to retrieve data, and mongoDB</p>
+      <p>made by leoni!</p>
+      <a href="https://github.com/lktay/second-fullstack-app">
+        <i class="fa-brands fa-github"></i>
+      </a>
+    </div>
+  );
+};
+
+export default About;
